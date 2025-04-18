c0de949d26ea0bac1b1b61846e503c97104fd747
Thierry Souverin <tsouverin@lpnhe.in2p3.fr>
2025-04-18 15:59:54 +0200

    bug absolute path

--- quick_look.py
+++ quick_look.py
@@ -326,7 +326,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    plot_dirpath = os.path.join("/home/tsouverin/polocalc/data_loader/20250417_165719/", "plots")
+    plot_dirpath = os.path.join(args.dirpath, "plots")
     print("All plots saved in : ", plot_dirpath)
     os.makedirs(plot_dirpath, exist_ok=False)
 
